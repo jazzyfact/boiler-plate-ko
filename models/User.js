71c6170611c12fc18eae51450f83a5cf2d96b5e2
ImhyeMi <jazzyfact2000@gmail.com>
2021-07-16 02:41:09 +0900

    [ADD] 토큰 생성후 POSTMAN으로 로그인 성공

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10 //10자리인 salt를 이용해 비밀번호를 암호화한다
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -56,6 +57,32 @@ userSchema.pre('save',function(next){
     }
 })
 
+
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    
+    //plainPassword 1234567 암호화된 비밀번호
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+            cb(null, isMatch)//에러는 없고 비밀번호는 같다
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+
+    const user = this;
+
+    //jsonwebtoken을 이용하여 토큰을 생성하기
+    const token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+            cb(null, user)
+    })
+
+   
+}
 
 
 
