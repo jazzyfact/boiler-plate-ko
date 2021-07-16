7a3c5a43ac302e11250152709cf08fe9595310dd
ImhyeMi <jazzyfact2000@gmail.com>
2021-07-17 00:30:32 +0900

    [ADD] CORS 이슈, Proxy 설정

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -27,6 +27,15 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => {
   res.send('Hello World!런런런111ffasdfasdf111')
 })
+
+
+
+app.get('/api/hello', (req, res) =>{
+  res.send("안녕하세요")
+})
+
+
+
 
 
 
@@ -115,6 +124,8 @@ app.get('/api/users/logout', auth, (req, res) => {
       )
 })
 
+
+const port = 5000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
