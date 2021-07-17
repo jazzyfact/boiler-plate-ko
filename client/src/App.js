ee5536f01ef52f4dc4921a9883ad6990721962c6
ImhyeMi <jazzyfact2000@gmail.com>
2021-07-17 13:38:24 +0900

    [ADD] Redux 이용해 로그인 페이지 만들기

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import {
   Link
 } from "react-router-dom";
 
-import LandingPage from './components/views/LandingPage/LandingPage'
-import LoginPage  from './components/views/LodingPage/LoginPage';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage  from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -11,8 +11,11 @@ function LadingPage(){
     }, [])
 
     return (
-        <div>
-            LadingPage gggg
+        <div style={{
+            display : 'flex', justifyContent: 'center', alignItems:'center',
+            width : '100%', height :'100vh'
+        }}> 
+           <h2>시작 페이지</h2>
         </div>
     )
 }
